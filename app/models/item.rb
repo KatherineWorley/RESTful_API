1ac9783f884cec3ddd6a800f71297c4a22ba369b
Katherine Worley <kdesign216@yahoo.com>
2018-05-23 06:29:42 -0400

    adding specs to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
 
   # validation
   validates_presence_of :name
+
+  FactoryBot.define do
+  factory :item do
+    name { Faker::StarWars.character }
+    done false
+    todo_id nil
+  end
+end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,4 +4,12 @@ class Todo < ApplicationRecord
 
   # validations
   validates_presence_of :title, :created_by
+
+  FactoryBot.define do
+  factory :todo do
+    title { Faker::Lorem.word }
+    created_by { Faker::Number.number(10) }
+  end
+end
+
 end
