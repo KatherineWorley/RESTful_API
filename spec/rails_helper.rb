02d7d2bd3dc50bdce92fffeb2698e4576f66c13d
Katherine Worley <kdesign216@yahoo.com>
2018-05-23 07:17:24 -0400

    adding routes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,27 +10,30 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 # [...]
 RSpec.configuration do |config|
-  # [...]
-  # add `FactoryBot` methods
-  config.include FactoryBot::Syntax::Methods
+    # [...]
+    # add `FactoryBot` methods
+    config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
+    # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.strategy = :transaction
     end
-  end
-  # [...]
-end
 
+    # start the transaction strategy as examples are run
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
+    # [...]
+
+    config.include RequestSpecHelper, type: :request
+end
 
 
 
